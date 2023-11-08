dac77e9c8248137e62368e9d0e93d5dbe788383b
Julie Foster <juliefoster6695@gmail.com>
2023-11-08 12:42:56 -0500

    added my rocks and delete for user

--- src/components/RockList.jsx
+++ src/components/RockList.jsx
@@ -1,7 +1,9 @@
 import { useEffect } from "react";
 
-export const RockList = ({ rocks, fetchRocks }) => {
-  useEffect(() => {}, []);
+export const RockList = ({ rocks, fetchRocks, showAll }) => {
+  useEffect(() => {
+    fetchRocks(showAll);
+  }, [showAll]);
 
   const displayRocks = () => {
     if (rocks && rocks.length) {
@@ -14,13 +16,38 @@ export const RockList = ({ rocks, fetchRocks }) => {
           <div>
             In the collection of {rock.user?.first_name} {rock.user?.last_name}
           </div>
+          {showAll ? (
+            ""
+          ) : (
+            <div>
+              <button
+                onClick={async () => {
+                  const response = await fetch(
+                    `http://localhost:8000/rocks/${rock.id}`,
+                    {
+                      method: "DELETE",
+                      headers: {
+                        Authorization: `Token ${
+                          JSON.parse(localStorage.getItem("rock_token")).token
+                        }`,
+                      },
+                    }
+                  );
+                  if (response.status === 204) {
+                    fetchRocks(showAll);
+                  }
+                }}
+                className="border border-solid bg-red-700 text-white p-1"
+              >
+                Delete
+              </button>
+            </div>
+          )}
         </div>
       ));
     }
-
     return <h3>Loading Rocks...</h3>;
   };
-
   return (
     <>
       <h1 className="text-3xl">Rock List</h1>
